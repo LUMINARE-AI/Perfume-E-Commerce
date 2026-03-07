8be386ea9561d7bb9866fd101d51cc9817ff6b70
Aditya Jain <adityajain907935@gmail.com>
2026-03-07 16:02:42 +0530

    refactor: simplify dotenv configuration and clean up unused code in index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app.js";
 import connectDB from "./db/index.js";
-
-dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
@@ -16,43 +16,3 @@ connectDB()
     console.error("❌ Failed to start server:", err);
     process.exit(1);
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import express from "express";
-const app = express();
-
-( async () => {
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERROR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port $ {process.env.PORT}`);
-        })
-
-    } catch (error){
-        console.error("ERROR: ", error);
-        throw err
-    }
-  }
-)()
-  */
